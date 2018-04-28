728b059fe0a807b710f12420e1ff9ff1fde6148f
Grant Wade <grant.wade@wsu.edu>
2018-04-27 23:08:06 -0700

    setup changes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/grant-wade/dotfiles.git
+git clone https://github.com/grant-wade/dotfiles.git .dotfiles
 
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/gww_custo.zsh-theme ~/.oh-my-zsh/themes/
+ln ~/.dotfiles/gww_custo.zsh-theme ~/.oh-my-zsh/themes/
