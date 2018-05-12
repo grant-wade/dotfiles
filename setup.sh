1b2b05ebc6b30b63ed291fcc5e3693f983c1bac3
Grant Wade <grant.wade@wsu.edu>
2018-05-12 13:51:55 -0700

    Updated setup.sh to install miniconda

--- setup.sh
+++ setup.sh
@@ -6,6 +6,15 @@ git clone https://github.com/grant-wade/dotfiles.git .dotfiles
 # Run setup script
 python2 ~/.dotfiles/install.py
 
+# Download miniconda install script
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Install miniconda
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# Remove miniconda installer
+rm Miniconda3-latest-Linux-x86_64.sh
+
 # Link all the needed files
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
